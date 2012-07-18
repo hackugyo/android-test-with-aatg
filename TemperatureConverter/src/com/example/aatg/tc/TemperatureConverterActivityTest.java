f72fbfc9ce893ed3f9de1efd75addf4459fa650b
kwatanabe <kwatanabe@kbmj.com>
2012-07-18 18:58:58 +0900

    make red in a japanese-name test method.

--- TemperatureConverter/src/com/example/aatg/tc/TemperatureConverterActivityTest.java
+++ TemperatureConverter/src/com/example/aatg/tc/TemperatureConverterActivityTest.java
@@ -9,6 +9,7 @@
 package com.example.aatg.tc;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
 /**
  * @author kwatanabe
@@ -19,6 +20,8 @@ public class TemperatureConverterActivityTest extends
 
     /** Creating Fixture */
     private TemperatureConverterActivity mActivity;
+    private EditText mCelsius;
+    private EditText mFahrenheit;
 
     /**
      */
@@ -40,6 +43,8 @@ public class TemperatureConverterActivityTest extends
     protected void setUp() throws Exception {
         super.setUp();
         mActivity = getActivity();
+        mCelsius    = (EditText)mActivity.findViewById(com.example.aatg.tc.R.id.celsius);
+        mFahrenheit = (EditText)mActivity.findViewById(com.example.aatg.tc.R.id.fahrenheit);
     }
 
     /* (非 Javadoc)
@@ -54,6 +59,19 @@ public class TemperatureConverterActivityTest extends
      */
     public final void testPreconditions() {
         assertNotNull(mActivity);
+        // assertThat()だと，Matcherクラスが取得できなかった．JUnit 3.0だからいかんのか
+        // Matcher<Object> matcher = is(not(nullValue()));
+        // assertThat(mActivity, matcher);
+    }
+
+
+    /**
+     * Testing the existence of the UI components
+     * 日本語のテスト名は利用できる．@Testアノテーションはだめだった
+     */
+    public final void testInputFieldsを持っている() {
+        assertNotNull(mCelsius);
+        assertNotNull(mFahrenheit);
     }
 
 }
