66262f1bfe564d6f28dc65de739687afa72c2a37
kwatanabe <kwatanabe@kbmj.com>
2012-07-18 18:14:52 +0900

    first commit

---
+++ TemperatureConverter/src/com/example/aatg/tc/TemperatureConverterActivityTest.java
@@ -0,0 +1,48 @@
+/**
+ * TemperatureConverterActivityTest.java
+ * TemperatureConverterTest
+ *
+ * Created by 株式会社 Appirits on 2012/07/18
+ * Copyright (c) 2012 Appirits. All rights reserved.
+ *
+ */
+package com.example.aatg.tc;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author kwatanabe
+ *
+ */
+public class TemperatureConverterActivityTest extends
+        ActivityInstrumentationTestCase2<TemperatureConverterActivity> {
+
+    /**
+     */
+    public TemperatureConverterActivityTest() {
+       this("TemperatureConverterActivityTest");
+    }
+
+    /**
+     * @param name
+     */
+    public TemperatureConverterActivityTest(String name) {
+        super(TemperatureConverterActivity.class);
+        setName(name);
+    }
+
+    /* (非 Javadoc)
+     * @see android.test.ActivityInstrumentationTestCase2#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /* (非 Javadoc)
+     * @see android.test.ActivityInstrumentationTestCase2#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
