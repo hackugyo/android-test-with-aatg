a9f332f4867fbed42d270775f27a35516f78ad4f
kwatanabe <kwatanabe@kbmj.com>
2012-07-19 15:01:40 +0900

    git add

---
+++ TemperatureConverterTested/src/com/example/aatg/tc/TemperatureConverterActivity.java
@@ -0,0 +1,13 @@
+package com.example.aatg.tc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TemperatureConverterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temperature_converter_main);
+    }
+}
